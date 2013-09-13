84510f62f44889c23d784a3afaeb00894461d56f
Dasol Lee <amdcpybt@gmail.com>
2013-09-14 04:47:16 +0900

    modified main index file

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 <html>
 
 <head>
+<link rel="stylesheet" type="text/css" href="dasol.css">
 <meta charset="UTF-8">
 <title>Dasol Lee</title>
 </head>
 
 <body>
-Welcome to P1
+<p><u>Name:</u> Dasol Lee</p>
+
+<p><u>A Small bio:</u> I am a 24 year old entrepreneur in Korea. <br>
+My company was recently funded by Google for Entrepreneurs Program. <br>
+I want to learn more about programming in order to become a better CEO at my company</p>
+
+<p><u>The code editor I am using:</u> Eclipse</p>
 </body>
 
 </html>
